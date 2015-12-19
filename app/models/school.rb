641feedcdcf82833b2d4f350ab57629a866f4366
robinfwzdev <robin@futureworkz.com>
2015-12-19 09:37:26 +0700

    Validates description on school model

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ActiveRecord::Base
   validates :name,   presence: true
+  validates :address,   presence: true
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe School do
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :address }
   end
 end
