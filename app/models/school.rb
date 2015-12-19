c5623e4f51b8712f52a2331b9e9cbb7f0b510cdb
robinfwzdev <robin@futureworkz.com>
2015-12-19 09:49:02 +0700

    Add enumerize type_of_school on school model

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,8 @@
 class School < ActiveRecord::Base
+  extend Enumerize
+
   validates :name,   presence: true
   validates :address,   presence: true
+
+  enumerize :type_of_school, in: [:university, :high_school, :secondary_school, :primary_school]
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -5,4 +5,8 @@ describe School do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :address }
   end
+
+  describe 'enumerize' do
+    it { should enumerize(:type_of_school).in(:university, :high_school, :secondary_school, :primary_school) }
+  end
 end
